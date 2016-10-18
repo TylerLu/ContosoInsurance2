1322a782f8912a841dffd46340b2b55225105eb9
TylerLu <admin@tyler.lu>
2016-10-18 12:11:22 +0800

    5

--- Src/Cloud/ContosoInsurance.API/Helpers/AuthenticationHelper.cs
+++ Src/Cloud/ContosoInsurance.API/Helpers/AuthenticationHelper.cs
@@ -11,19 +11,9 @@ namespace ContosoInsurance.API.Helpers
     {
         internal static async Task<string> GetUserIdAsync(HttpRequestMessage request, IPrincipal user)
         {
-            //var creds = await GetCurrentCredentialAsync(request, user);
-            //if (creds == null) return null;
-            //return $"{creds.Provider}:{creds.Claims.GetValue(ClaimTypes.NameIdentifier)}";
-
-            ClaimsPrincipal claimsUser = (ClaimsPrincipal)user;
-
-            string provider = claimsUser.FindFirst("http://schemas.microsoft.com/identity/claims/identityprovider").Value;
-            string sid = claimsUser.FindFirst(ClaimTypes.NameIdentifier).Value;
-
-            // The above assumes WEBSITE_AUTH_HIDE_DEPRECATED_SID is true. Otherwise, use the stable_sid claim:
-             //sid +=";" + claimsUser.FindFirst("stable_sid").Value; 
-
-            return $"{provider}|{sid}";
+            var creds = await GetCurrentCredentialAsync(request, user);
+            if (creds == null) return null;
+            return $"{creds.Provider}:{creds.Claims.GetValue(ClaimTypes.NameIdentifier)}";
         }
 
         internal static async Task<ProviderCredentials> GetCurrentCredentialAsync(HttpRequestMessage request, IPrincipal user)
